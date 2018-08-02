f613a709f6869e4538dbf1efdbbcf4f22b557982
caoyong <1666416683@qq.com>
2018-08-02 16:39:13 +0800

    商家管理获取停车场列表方法修改

--- pages/sellerManage/sellerManage.js
+++ pages/sellerManage/sellerManage.js
@@ -14,17 +14,18 @@ Page({
   onShow() {
     var that = this
     var openId = app.globalData.openId
-    console.log("进入停车场管理页面===", app.globalData.url + '/park/parkOwnerList/openid/' + openId)
+    console.log("商家进入停车场管理页面", app.globalData.url + '/car/weixin/seller/parkList/openid/' + openId)
+
     wx.request({
-      url: app.globalData.url + '/park/parkOwnerList/openid/' + openId,
+      url: app.globalData.url + '/car/weixin/seller/parkList/openid/' + encodeURI(openId),
       method: 'GET',
       data: {},
       header: {
         "content-type": 'application/x-www-form-urlencoded'
       },
       success: function (res) {
-        console.log("调用车场管理接口成功")
-        console.log("array====>", res)
+        console.log("调用商家停车场管理接口成功")
+        console.log("parkList====>", res)
         if (res.data != '') {
           that.setData({
             array: res.data.data
