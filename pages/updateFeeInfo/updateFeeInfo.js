ee69cf07bb17f5c281b64ace088a16e12946f2de
寒冰 <2487825637@qq.com>
2018-08-06 15:12:32 +0800

    更新： 1. 更新收费规则的修改规则。前后端同时进行数据校验

--- pages/updateFeeInfo/updateFeeInfo.js
+++ pages/updateFeeInfo/updateFeeInfo.js
@@ -38,12 +38,40 @@ Page({
       }
     })
   },
+  unitMinuteInput: function (e) {
+    var unitMinute = e.detail.value;
+    if (unitMinute.length == 0) {
+      wx.showToast({
+        title: '单位时间不可为空！',
+        icon: 'none',
+        duration: 2000
+      })
+    } else if (unitMinute == 0) {
+      wx.showToast({
+        title: '单位时间不可为0！',
+        icon: 'none',
+        duration: 2000
+      })
+
+    } else if (unitMinute < 0) {
+      wx.showToast({
+        title: '单位时间不可小于0！',
+        icon: 'none',
+        duration: 2000
+      })
+
+    }
+    this.setData({
+      focus: true
+    })
+  },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e)
+
     var data = e.detail.value;
     var id = e.detail.target.dataset.parkId;
     var startMinute = data.startMinute;
-    if (startMinute==''){
+    if (startMinute == '') {
       startMinute = 0;
     }
     var startPrice = data.startPrice;
@@ -51,58 +79,82 @@ Page({
       startPrice = 0;
     }
     var feeTop = data.feeTop;
-    if(feeTop==''){
+    if (feeTop == '') {
       feeTop = 0;
     }
     var freeMinute = data.freeMinute;
-    if (freeMinute==''){
+    if (freeMinute == '') {
       freeMinute = 0;
-    } 
-    var feeMonth = data.feeMonth;
-    if(feeMonth==''){
-      feeMonth = 0;
     }
-    var unitMinute = data.unitMinute;
-    if (unitMinute == '') {
-      unitMinute = 1;
+    var feeMonth = data.feeMonth;
+    if (feeMonth == '') {
+      feeMonth = 0;
     }
     var unitPrice = data.unitPrice;
     if (unitPrice == '') {
       unitPrice = 0;
     }
 
-    wx.request({
-      url: app.globalData.url + '/park/updatePark/id/' + id + '/startMinute/' + startMinute + '/startPrice/' + startPrice + '/feeTop/' + feeTop + '/freeMinute/' + freeMinute + '/feeMonth/' + feeMonth + '/unitMinute/' + unitMinute +'/unitPrice/'+unitPrice,
-      method: 'GET',
-      data: {
-      },
-      header: {
-        "content-type": 'application/x-www-form-urlencoded'
-      },
-      success: function (res) {
-        console.log("调用修改计费规则接口成功")
-        console.log("array====>", res)
-        wx.redirectTo({
-          url: '/pages/index/index',
-          success: function (res) {
-            wx.showToast({
-              title: '保存成功',
-              icon: 'success',
-              duration: 2000
-            })
-          }
-        })
-      },
-      fail: function (res) {
-        console.log("调用修改计费规则接口失败")
-        console.log("失败原因====>", res)
+    var unitMinute = data.unitMinute;
+    if (unitMinute == "" || unitMinute == undefined || unitMinute < 0 || unitMinute == 0) {
+      if (unitMinute.length == 0) {
         wx.showToast({
-          title: '保存失败',
+          title: '单位时间不可为空！',
           icon: 'none',
           duration: 2000
         })
+      } else if (unitMinute == 0) {
+        wx.showToast({
+          title: '单位时间不可为0！',
+          icon: 'none',
+          duration: 2000
+        })
+
+      } else if (unitMinute < 0) {
+        wx.showToast({
+          title: '单位时间不可小于0！',
+          icon: 'none',
+          duration: 2000
+        })
+
       }
-    })
+      this.setData({
+        focus: true
+      })
+    } else {
+      unitMinute = unitMinute;
+      wx.request({
+        url: app.globalData.url + '/park/updatePark/id/' + id + '/startMinute/' + startMinute + '/startPrice/' + startPrice + '/feeTop/' + feeTop + '/freeMinute/' + freeMinute + '/feeMonth/' + feeMonth + '/unitMinute/' + unitMinute + '/unitPrice/' + unitPrice,
+        method: 'GET',
+        data: {},
+        header: {
+          "content-type": 'application/x-www-form-urlencoded'
+        },
+        success: function (res) {
+          console.log("调用修改计费规则接口成功")
+          console.log("array====>", res)
+          wx.redirectTo({
+            url: '/pages/index/index',
+            success: function (res) {
+              wx.showToast({
+                title: '保存成功',
+                icon: 'success',
+                duration: 2000
+              })
+            }
+          })
+        },
+        fail: function (res) {
+          console.log("调用修改计费规则接口失败")
+          console.log("失败原因====>", res)
+          wx.showToast({
+            title: '保存失败',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      })
+    }
 
   },
 })
