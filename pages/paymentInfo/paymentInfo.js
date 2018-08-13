efbc698e5e77105bcd57c0c419f8e5669b1d0f50
寒冰 <2487825637@qq.com>
2018-08-13 13:14:52 +0800

    更新： 1. 微信小程序段仅显示用户前500条记录剩余记录，避免数据过大无法显示数据。

--- pages/paymentInfo/paymentInfo.js
+++ pages/paymentInfo/paymentInfo.js
@@ -2,9 +2,11 @@ const app = getApp()
 
 Page({
   data: {
-    paymentInfo: {},
-    plateNum: null
+    paymentInfo: [],
+    plateNum: null,
+    
   },
+ 
   onLoad(res) {
     console.log('车牌号为：', res.plateNum)
     this.setData({
@@ -22,19 +24,31 @@ Page({
       success: function (res) {
         console.log("调用接口成功")
         console.log("在线缴费记录====>", res)
+        var a=0;
         if(res.data.data){
+          var ss = [];
+          for(a=0;a<500;a++){
+            // ss=[];
+            ss.push(res.data.data[a]);
+            
+          }
           that.setData({
-            paymentInfo: res.data.data
+            // pageIndex:that.data.pageIndex+1,
+            paymentInfo: ss
           })
+          // console.log("paymentInfo=================================>" + that.data.paymentInfo)
         }else{
           that.setData({
             paymentInfo: ''
           })
+          // console.log("paymentInfo=================================>" + that.paymentInfo)
         }
       }
     })
       wx.setNavigationBarTitle({
         title: '历史记录'
       })
-  }
+  },
+ 
+  
 })
